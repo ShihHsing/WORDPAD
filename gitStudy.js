3478d8ef2f9df1dc9005e0b6d32b46958604c7bb
Drama <abandoneddrama@foxmail.com>
2017-03-29 00:22:21 +0800

    在家学习

--- gitStudy.js
+++ gitStudy.js
@@ -18,3 +18,4 @@ git remote add origin git@server-name:path/repo-name.git // 关联远程仓库
 git push -u origin master // 第一次推送master分支的所有内容
 git oush origin master // 推送最新修改
 git checkout -- <fileName> // 用版本库里的版本替换工作区的版本 == 一键还原
+git clone github@url // 从远程库拷贝项目到本地文件夹
